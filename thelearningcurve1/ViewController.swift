632c31d70e8404cc1ed8f8494a1a5d9014af6bd2
Vivek P Nair <vivek@cosmichq.com>
2015-01-25 01:07:18 +0530

    Initial Commit

---
+++ thelearningcurve1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  thelearningcurve1
+//
+//  Created by Vivek Nair on 1/25/15.
+//  Copyright (c) 2015 Vivek Nair. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
